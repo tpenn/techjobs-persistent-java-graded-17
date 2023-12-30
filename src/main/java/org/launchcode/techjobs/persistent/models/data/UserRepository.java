836f289518d4b65d7cbe4784867b174761bdb6f3
Terri Penn <coderterri@protonmail.com>
2023-12-30 08:26:42 -0600

    Add UserRepository

---
+++ src/main/java/org/launchcode/techjobs/persistent/models/data/UserRepository.java
@@ -0,0 +1,8 @@
+package org.launchcode.techjobs.persistent.models.data;
+
+import org.launchcode.techjobs.persistent.models.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+    User findByUsername(String username);
+}
